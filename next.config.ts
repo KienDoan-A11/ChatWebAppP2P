8157b17ccece27be156576afc21d407e2c6e94d2
Qanh <114667223+QucAnh@users.noreply.github.com>
2024-12-09 16:02:19 +0700

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
